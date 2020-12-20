436f18b197dea7b208474f082506d7d116cadadb
Jabagh97 <76058182+Jabagh97@users.noreply.github.com>
2020-12-20 23:25:31 +0300

    Update DBConnection.java

--- RestaurantManagementSystem/src/database/DBConnection.java
+++ RestaurantManagementSystem/src/database/DBConnection.java
@@ -1,137 +1,72 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import main.MD5;
+
 
 public class DBConnection {
-	
-	private Connection conn = null;
-	
-	
-	public DBConnection(String url) throws SQLException {
-		
-		String[] arg = url.split("[,]");
-	    ArrayList<String> list = new ArrayList<String> (Arrays.asList(arg));
-	    //list(0) = url, list(1) = username, list(2) = password
-			conn = DriverManager.getConnection(list.get(0),list.get(1),list.get(2));
-	}
-	
-	public void close() {
-		if(conn != null) {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-			}
-		}
-	}
 
-	
-	
-	/**
-	 * A method to send select statement to the underlying DBMS (e.g., "select * from Table1")
-	 * @param query_statement A query to run on the underlying DBMS 
-	 * @return Resultset the query result. 
-	 */
-	public ResultSet send_query(String query_statement) {
-		// Feel free to make them Class attributes
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-		    stmt = conn.createStatement();
-		    rs = stmt.executeQuery(query_statement);
-		    printData(stmt,rs);
-		}
-		catch (SQLException ex){
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-		finally {
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/project";
+    private static final String USER = "root";
+    private static final String PASS = "159357";
 
-		    if (rs != null) {
-		        try {
-		            rs.close();
-		        } catch (SQLException sqlEx) { } // ignore
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    private static PreparedStatement pstmt = null;
 
-		        rs = null;
-		    }
+    public void init() {
+        try {
 
-		    if (stmt != null) {
-		        try {
-		            stmt.close();
-		        } catch (SQLException sqlEx) { } // ignore
-
-		        stmt = null;
-		    }
-		}	
-	    return rs;
-		
-	}
-	
-
-	public ArrayList<String> getData(String query) throws SQLException {
-		 Statement stmt = conn.createStatement();
-		 ResultSet rs = stmt.executeQuery(query);
-		 
-		 ResultSetMetaData rsmd = rs.getMetaData();
-		 int columnsNumber = rsmd.getColumnCount();
-
-		ArrayList<String> readData = new ArrayList<String>();
-
-		while (rs.next()) {       
-			for(int i = 1 ; i <= columnsNumber; i++) {
-				readData.add(rs.getString(i));
-			}
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+            System.out.println("connection initialized");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Problem!");
         }
-		return readData;
-	} 
 
-	
-	
-	public void printData(Statement st, ResultSet rs) throws SQLException {
-		ResultSetMetaData rsmd = rs.getMetaData();
-		int columnsNumber = rsmd.getColumnCount();
-		// Iterate through the data in the result set and display it.
-		while (rs.next()) {
-			//Print one row          
-			for(int i = 1 ; i <= columnsNumber; i++){
-				if(i < columnsNumber) {
-					System.out.print(rs.getString(i) + ","); //Print one element of a row
-					
-				}
-				else {
-					System.out.print(rs.getString(i));
-				}
-			}
-			System.out.println();//Move to the next line to print the next row.           
-		}
-	} 
+    }//end try
 
+
+    public boolean getManager(String username, String pass) {
+        Boolean var = false;
+
+        try {
+            pstmt = conn.prepareStatement("SELECT * FROM manager WHERE mUserName=? AND passWord=?");
+            pstmt.setString(1, username);
+            pstmt.setString(2, pass);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+
+                if (rs.next())
+                    var = true;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return var;
+    }
+
+
+    public boolean getEmployee(String username, String pass) {
+        Boolean var = false;
+
+        try {
+            pstmt = conn.prepareStatement("SELECT * FROM employee WHERE eUserName=? AND passWord=?");
+            pstmt.setString(1, username);
+            pstmt.setString(2, pass);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                var = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return var;
+    }
 	
-	
-	// TODO Write a method to insert new data into the DBMS
-	public  void post(String sql) throws Exception {
-		try {
-//			PreparedStatement posted = conn.prepareStatement("USE ;");
-//			posted.executeUpdate();
-			
-			PreparedStatement posted = conn.prepareStatement(sql);
-			posted.executeUpdate();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-//		finally {
-//			System.out.println("Insert Completed");
-//		}
-	}
+	//rest of of functions should be implemented here
 
 }
