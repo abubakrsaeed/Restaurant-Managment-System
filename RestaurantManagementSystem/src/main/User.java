54aeebf09c61e5b5e4bb43ca8577e8392d3d23c4
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2020-12-24 12:25:37 +0300

    Setters and getters added

--- RestaurantManagementSystem/src/main/User.java
+++ RestaurantManagementSystem/src/main/User.java
@@ -1,27 +1,51 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import database.DBConnection;
-
-
 public class User {
-	
-	private static String username;
-	private String password;
-	private static DBConnection con;
-	
-	
-	public User(String username_, String password_, DBConnection con_) {
-		username = username_;
-		password = password_;
-		con = con_;
-	}
-	
 
-	
+	private int id;
+	private String fullName;
+	private String username;
+	private String password;
+	private String userType;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getUserType() {
+		return userType;
+	}	
+
 }
