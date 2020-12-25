a2d542450edeb13160771a7c07d0872b78ed6fe8
Jabagh Anjouk <76058182+Jabagh97@users.noreply.github.com>
2020-12-25 14:37:55 +0300

    Update DBConnection.java

--- RestaurantManagementSystem/src/database/DBConnection.java
+++ RestaurantManagementSystem/src/database/DBConnection.java
@@ -1,72 +1,102 @@
 package database;
 
-import java.sql.*;
-import java.util.ArrayList;
-import main.MD5;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+//import main.User; 
 
 public class DBConnection {
 
-    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/project";
-    private static final String USER = "root";
-    private static final String PASS = "159357";
-
-    private static Connection conn = null;
-    private static Statement stmt = null;
-    private static PreparedStatement pstmt = null;
-
-    public void init() {
-        try {
-
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();
-            System.out.println("connection initialized");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Problem!");
-        }
-
-    }//end try
-
-
-    public boolean getManager(String username, String pass) {
-        Boolean var = false;
-
-        try {
-            pstmt = conn.prepareStatement("SELECT * FROM manager WHERE mUserName=? AND passWord=?");
-            pstmt.setString(1, username);
-            pstmt.setString(2, pass);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-
-                if (rs.next())
-                    var = true;
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return var;
-    }
-
-
-    public boolean getEmployee(String username, String pass) {
-        Boolean var = false;
-
-        try {
-            pstmt = conn.prepareStatement("SELECT * FROM employee WHERE eUserName=? AND passWord=?");
-            pstmt.setString(1, username);
-            pstmt.setString(2, pass);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                var = true;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return var;
-    }
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Connection conn = createConnection();
+////		createNewUser(conn, 456, "Lahore", 6445);
+//	}
+	public static Connection createConnection() {
+		
+		String con_driver = "com.mysql.jdbc.Driver";
+		String con_localHost = "localhost:3306";
+		String con_databaseName = "rms_database";
+		String con_userName = "root";
+		String con_password = "";
 	
-	//rest of of functions should be implemented here
+		Connection connection = null;
+		try {
+			Class.forName(con_driver);
+			connection=DriverManager.getConnection("jdbc:mysql://"+con_localHost+"/"+con_databaseName+"",con_userName,con_password);	
+		}catch(Exception e1) {
+			System.out.println(e1);
+		}
+		System.out.println(connection);
+		return connection;
+	}
+	
+	public static void createNewUser(Connection conn, String name, String username, String password, String usertype) {
+		try {
+			PreparedStatement ps = conn.prepareStatement("insert into tbl_users values(?,?,?,?);");
+			
+			ps.setString(1, name);
+			ps.setString(2, username);
+			ps.setString(3, password);
+			ps.setString(4, usertype);
 
+			//check x?
+			int x = ps.executeUpdate();
+			if (x > 0) {
+				System.out.println("Successful");
+			} else {
+				System.out.println("Not successful");
+			}
+		} catch(Exception e1) {
+			System.out.println(e1);
+		}
+	}
+
+//	public static boolean login(String userName, String password) {	
+//		return true;
+//	}
+//	
+	
+	public String authenticateUser(Connection con, String username, String password, String usertype)
+	{
+	    
+		//copy string parameters
+		
+	    Statement statement = null;
+	    ResultSet resultSet = null;
+	 
+	    String usernameDB = "";
+	    String passwordDB = "";
+	    String usertypeDB = "";
+	 
+	    try
+	    {
+	        statement = con.createStatement();
+	        resultSet = statement.executeQuery("SELECT username, password, usertype FROM users");
+	 
+	        while(resultSet.next())
+	        {
+	        	usernameDB = resultSet.getString("username");
+	            passwordDB = resultSet.getString("password");
+	            usertypeDB = resultSet.getString("usertype");
+	 
+	            if(username.equals(usernameDB) && password.equals(passwordDB) && usertypeDB.equals("admin"))
+	            	return "Manager";
+	            else if(username.equals(usernameDB) && password.equals(passwordDB) && usertypeDB.equals("employee"))
+	            	return "Employee";
+	        }
+	    }
+	    catch(SQLException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    return "Invalid user credentials";
+	}
+	
+	
+	
 }
