e016bfd6b1c21d091745b0addf1f342209f8f8cf
asadrana7 <76475574+asadrana7@users.noreply.github.com>
2020-12-30 21:25:40 +0300

    Update Manager.java

--- RestaurantManagementSystem/src/main/Manager.java
+++ RestaurantManagementSystem/src/main/Manager.java
@@ -1,105 +1,41 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import database.DBConnection;
 
-public class Manager {
+public class Manager extends User {
+	
+	public Manager(String name, String username, String password, int salary, String usertype, DBConnection dbObj) {
+		super(name, username, password, salary, usertype, dbObj);
+	}
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub`
-
-		User user = new User();
-		
-		user.setFullName(getName());
-		//System.out.println(user.getFullName());
-		
-		user.setUsername(getUsername());
-		//System.out.println(user.getUsername());
-		
-		user.setPassword(getPassword());
-		//System.out.println(user.getPassword());
-		
-		user.setUserType(getUserType());
-		//System.out.println(user.getUserType());
-		
-		
-		
-//		//test
-//		DBConnection lp = new DBConnection();
-//		
-//		Connection conn = lp.createConnection();
-//		
-//		//lp.createNewUser(conn, 123, user.getUsername(), 123123);
-//		lp.createNewUser(conn, user.getFullName(), user.getUsername(), user.getPassword(), user.getUserType());
-//		String auth = lp.authenticateUser(conn, "admin", "admin", "admin");		
-//		//String auth = lp.authenticateUser(conn, user);
-//		System.out.println(auth);
-		
+	public boolean deleteItem(int id, String table) {
+		if (getDB().searchItem(id, table)) 
+			return getDB().deleteItem(id, table); 
+		else return false;
 	}
 	
-	public static String getName() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-		//needs changing during integration
-		System.out.print("Full name: ");
-	    return reader.readLine(); 
-	}
-	
-	public static String getUsername() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Username: ");
-		return reader.readLine();
-	}
-	
-	public static String getPassword() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-		System.out.print("Password: ");
-	    return reader.readLine();
-	}
-	
-	public static String getUserType() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-		System.out.print("User type (admin/employee): ");
-	    return reader.readLine(); 
-	}
-	
-	public static void createNewItem(Connection con, String id, String name, int price) {
-		try {
-			PreparedStatement ps = con.prepareStatement("INSERT INTO menu VALUES (?,?,?);");
+	public boolean createNewItem(int id, String name, int price) {
+		if ( !getDB().searchItem(id, "menu") ) 
+			return getDB().createNewItem(id, name, price);
+		else return false;
+	}	
 
-			ps.setString(1, id);
-			ps.setString(2, name);
-			ps.setInt(3, price);
-
-			if (ps.executeUpdate() == 0) {
-				System.out.println("Create new item unsuccessfull.");
-			} else {
-				System.out.println("Create new item successfull.");
-			}
-		} catch(Exception e) {
-			System.out.println(e);
-		}
-	}
-	public static int deleteItem(Connection con, int id) {
-
-		int param = 0;
-		try {
-			PreparedStatement ps = con.prepareStatement("DELETE FROM menu WHERE id LIKE;");
-			ps.setInt(1, id);
-			int exec = ps.executeUpdate();
-			param = exec;
-			System.out.println(exec);
-			if (ps.executeUpdate() == 0) {
-				System.out.println("Item deletion unsuccessfull.");
-				return param;
-			} else {
-				System.out.println("Item deletion successfull");
-				return param;
-			}
-		} catch(Exception e) {	System.out.println(e);	return param; }
+	public boolean deleteItemFromMenu(int id) {
+		return deleteItem(id, "menu");
+//		if ( getDB().searchItem(id, "menu") ) 
+//			return getDB().deleteItem(id, "menu");
+//		else return false;
 	}
 
+	public boolean updateItem(int id, String name, int price) {
+		if ( getDB().searchItem(id, "menu") ) 
+			return getDB().updateItem(name, price, id);
+		else return false;
+	}
+
+	
 }
