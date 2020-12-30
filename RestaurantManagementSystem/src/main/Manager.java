8476304bd70b9a754981cdcedf2d35a4513260b7
Jabagh Anjouk <76058182+Jabagh97@users.noreply.github.com>
2020-12-30 22:26:07 +0300

    Update Manager.java
    (add new employee, delete employee, update employee) still needs modifications to the following functions in the database class so the functions work

--- RestaurantManagementSystem/src/main/Manager.java
+++ RestaurantManagementSystem/src/main/Manager.java
@@ -26,15 +26,62 @@ public class Manager extends User {
 
 	public boolean deleteItemFromMenu(int id) {
 		return deleteItem(id, "menu");
-//		if ( getDB().searchItem(id, "menu") ) 
-//			return getDB().deleteItem(id, "menu");
-//		else return false;
+		if ( getDB().searchItem(id, "menu") ) 
+			return getDB().deleteItem(id, "menu");
+		else return false;
 	}
 
 	public boolean updateItem(int id, String name, int price) {
 		if ( getDB().searchItem(id, "menu") ) 
 			return getDB().updateItem(name, price, id);
 		else return false;
+	
+	public boolean createNewEmployee(String name, String username, String password, int salary) {
+		if ( !getDB().searchEmployee(username) ) 
+			return getDB().createNewUser(name, username, password, salary, "employee");
+		else return false;
+	}
+	
+	
+
+	public boolean deleteEmployee(String username) {
+		if ( getDB().searchEmployee(username) ) 
+			return getDB().deleteEmployee(username);
+		else return false;
+	}
+	
+
+	public boolean updateEmployee(String name,  String password, int salary, String update_username) {
+		if ( getDB().searchEmployee(update_username) ) 
+			return getDB().updateEmployee(name, password, salary, "employee", update_username);
+		else return false;
+	}
+	
+	
+	public String[][] getAllEmployees() throws SQLException {
+
+		ResultSet rs = getDB().getAllEmployees();
+
+		ArrayList<String[]> rowList = new ArrayList<String[]>();
+		int r = 0;
+
+		try {
+			while(rs.next()) {
+				rowList.add(new String[] { rs.getString("name"), rs.getString("username"), String.valueOf(rs.getInt("salary")) });
+				r++;
+			}
+		} catch (SQLException e) { e.printStackTrace(); }
+
+		int c = 3;
+		return makeTable(rowList, r, c);
+
+				String[][] data =  new String[r][c];
+			
+				for (int i = 0; i < r; i++)
+					for (int j = 0; j < c; j++)
+						data[i][j] = rowList.get(i)[j];
+
+				return data;
 	}
 
 	
