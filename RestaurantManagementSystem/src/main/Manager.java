c81e839cb361b6fb1472418181bd41569d713ded
Jabagh Anjouk <76058182+Jabagh97@users.noreply.github.com>
2020-12-31 01:42:36 +0300

    Update Manager.java
    Updated the  manager functions the mainly uses the DB connection functions

--- RestaurantManagementSystem/src/main/Manager.java
+++ RestaurantManagementSystem/src/main/Manager.java
@@ -8,81 +8,90 @@ import database.DBConnection;
 
 public class Manager extends User {
 	
-	public Manager(String name, String username, String password, int salary, String usertype, DBConnection dbObj) {
-		super(name, username, password, salary, usertype, dbObj);
+	public Manager(String name, String username, String password, int salary, String usertype) {
+		super(name, username, password, salary, usertype);
 	}
 
 	public boolean deleteItem(int id, String table) {
-		if (getDB().searchItem(id, table)) 
-			return getDB().deleteItem(id, table); 
-		else return false;
+		DBConnection db= new DBConnection();
+		db.init();
+		boolean var=db.deleteItem(id, table);
+		return var;
 	}
 	
 	public boolean createNewItem(int id, String name, int price) {
-		if ( !getDB().searchItem(id, "menu") ) 
-			return getDB().createNewItem(id, name, price);
-		else return false;
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		boolean var=db.createNewItem(id, name, price);
+		return var;
 	}	
 
-	public boolean deleteItemFromMenu(int id) {
-		return deleteItem(id, "menu");
-		if ( getDB().searchItem(id, "menu") ) 
-			return getDB().deleteItem(id, "menu");
-		else return false;
-	}
-
-	public boolean updateItem(int id, String name, int price) {
-		if ( getDB().searchItem(id, "menu") ) 
-			return getDB().updateItem(name, price, id);
-		else return false;
-	
-	public boolean createNewEmployee(String name, String username, String password, int salary) {
-		if ( !getDB().searchEmployee(username) ) 
-			return getDB().createNewUser(name, username, password, salary, "employee");
-		else return false;
-	}
-	
 	
 
-	public boolean deleteEmployee(String username) {
-		if ( getDB().searchEmployee(username) ) 
-			return getDB().deleteEmployee(username);
-		else return false;
+	public boolean updateItem(String name, int price, int update_id) {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		boolean var=db.updateItem( name, price,update_id);
+		
+		return var;
 	}
+		
 	
-
-	public boolean updateEmployee(String name,  String password, int salary, String update_username) {
-		if ( getDB().searchEmployee(update_username) ) 
-			return getDB().updateEmployee(name, password, salary, "employee", update_username);
-		else return false;
+	boolean createNewEmployee(String name, String username, String password, int salary,String usertype) {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		boolean var=db.createNewUser(name, username, password, salary, usertype);
+		
+		return var;
 	}
 	
 	
-	public String[][] getAllEmployees() throws SQLException {
 
-		ResultSet rs = getDB().getAllEmployees();
-
-		ArrayList<String[]> rowList = new ArrayList<String[]>();
-		int r = 0;
-
-		try {
-			while(rs.next()) {
-				rowList.add(new String[] { rs.getString("name"), rs.getString("username"), String.valueOf(rs.getInt("salary")) });
-				r++;
-			}
-		} catch (SQLException e) { e.printStackTrace(); }
-
-		int c = 3;
-		return makeTable(rowList, r, c);
-
-				String[][] data =  new String[r][c];
-			
-				for (int i = 0; i < r; i++)
-					for (int j = 0; j < c; j++)
-						data[i][j] = rowList.get(i)[j];
-
-				return data;
+	boolean deleteEmployee(String username) {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		boolean var=db.deleteEmployee(username);
+		
+		return var;
 	}
+	
+	
+	boolean updateEmployee(String name,  String password, int salary,String usertype ,String update_username) {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		boolean var=db.updateEmployee(name, password, salary, usertype, update_username);
+		
+		return var;
+	}
+	
+	
+	public ArrayList<ArrayList<String>> getAllEmployees()  {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		ArrayList<ArrayList<String>> var=db.getAllEmployees();
+		
+		return var;
+		
+		
+	}
+	public ArrayList<ArrayList<String>> getAllItems()  {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		ArrayList<ArrayList<String>> var=db.getAllItems();
+		
+		return var;
+		
+		
+	}
+
+	
 
 	
 }
