0bc11e94e2f483c8fb557dae430a9f10b9ce34b5
Jabagh Anjouk <76058182+Jabagh97@users.noreply.github.com>
2020-12-31 02:44:10 +0300

    Update Employee.java
    Employee almost done

--- RestaurantManagementSystem/src/main/Employee.java
+++ RestaurantManagementSystem/src/main/Employee.java
@@ -6,8 +6,8 @@ import database.DBConnection;
 
 public class Employee extends User {
 
-	public Employee(String name, String username, String password, int salary, String usertype, DBConnection dbObj) {
-		super(name, username, password, salary, usertype, dbObj);
+	public Employee(String name, String username, String password, int salary, String usertype) {
+		super(name, username, password, salary, usertype);
 	}
 
 	//helper
@@ -18,39 +18,30 @@ public class Employee extends User {
 	}
 
 
-	public int updatePrice(int qty, int price) {
-		return qty * price;
+	public boolean updatePrice(String name, int price, int update_id) {
+		DBConnection db= new DBConnection();
+		db.init();
+		
+		boolean var=db.updateItem( name, price,update_id);
+		
+		return var;
 	}
 
 	public void createOrder() {
+		DBConnection db= new DBConnection();
+		db.init();
 		
-		int total = 0;
 		
-		int orderNo = Integer.parseInt(generateRandomString());
-		while (!getDB().checkOrderNo(orderNo)) { orderNo = Integer.parseInt(generateRandomString()); }
-
-		Menu order = new Menu(orderNo, this);
+		//those should been taken from the GUI
+		int orderNo = 0; Employee obj = null;int qty = 0;String name = null;
 		
-		String command=""; //from gui
-
-		if (command.equals("new")) {
-			//first list all items
-			//get name and qty from user (radio button or whatever)
-			order.addOrder( name, qty);
-			total += updatePrice(qty, getDB().getPrice(name, "menu"));
-		}
-		else if (command.equals("update")) {
-			order.viewCurrentOrder();
-			//get id and qty from the above view
-			order.updateOrderQty(id, qty);
-		}
-		else if (command.equals("delete")) {
-			order.viewCurrentOrder();
-			//get id from user
-			order.deleteOrder(id);
-		}
-		else if (command.equals("submit")) {
-			//destroy the menu object here and close
-		}
+		String placedBy=obj.getlName();
+		
+		Order n=new Order(orderNo,obj,qty,name);
+		
+		db.createNewOrder(orderNo, name, qty, placedBy);
+		
+		
+	
 	}
 }
